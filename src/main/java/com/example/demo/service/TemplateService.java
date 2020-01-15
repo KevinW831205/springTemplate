9c434bb7ee700d89515f1044037dd0ee1e4a9c73
Kevin Wang <bird831205@hotmail.com>
2020-01-15 14:23:39 -0500

    service class

--- src/main/java/com/example/demo/service/TemplateService.java
+++ src/main/java/com/example/demo/service/TemplateService.java
@@ -1,4 +1,15 @@
 package com.example.demo.service;
 
+import com.example.demo.repository.TemplateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TemplateService {
+    private TemplateRepository templateRepository;
+
+    @Autowired
+    public TemplateService(TemplateRepository templateRepository) {
+        this.templateRepository = templateRepository;
+    }
 }
