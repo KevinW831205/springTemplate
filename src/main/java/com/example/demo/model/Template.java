7073931f320599725b6c8b7ec6b96bc66a590403
Kevin Wang <bird831205@hotmail.com>
2020-01-15 14:44:34 -0500

    model enitity

--- src/main/java/com/example/demo/model/Template.java
+++ src/main/java/com/example/demo/model/Template.java
@@ -1,5 +1,42 @@
 package com.example.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Template {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String field1;
+    private String field2;
+
+    public Template() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getField1() {
+        return field1;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public void setField2(String field2) {
+        this.field2 = field2;
+    }
 }
